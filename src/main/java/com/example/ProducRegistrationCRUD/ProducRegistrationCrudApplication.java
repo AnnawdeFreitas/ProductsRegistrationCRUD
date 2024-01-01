42a927028f9de2aec412b41223c29f1d85f7f601
Anna Waleska de Freitas <annawrdefreitas@gmail.com>
2024-01-01 16:30:22 -0300

    update the project with initial setup

---
+++ src/main/java/com/example/ProducRegistrationCRUD/ProducRegistrationCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProducRegistrationCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducRegistrationCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducRegistrationCrudApplication.class, args);
+	}
+
+}
